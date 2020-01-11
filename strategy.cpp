613b410c19ec5bc0ccf949efcfa1dbb320d2e45f
Eric Rippey <rippey.e@gmail.com>
2020-01-10 22:22:06 -0800

    Version that could be used in competition

--- strategy.cpp
+++ strategy.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+/*To do list:
+ * 1) output example input file
+ * 2) make the availability of the bonus objectives probabilistic - joint probability distribution
+ * 3) different strategies about who is climbing effects time to do other things
+ * 4) Improve quality of random robot match data
+ * */
+
 //start generic code
 
 template<int MIN,int MAX>
@@ -18,7 +25,12 @@ class Int_limited{
 
 	operator int()const{ return i; }
 
-	//bool operator<(Int_limited)const;
+	Int_limited operator--(int){
+		auto r=*this;
+		i--;
+		assert(i>=MIN);
+		return r;
+	}
 };
 
 template<int MIN,int MAX>
@@ -147,9 +159,148 @@ string take(size_t n,string s){
 	return s.substr(0,n);
 }
 
+int parse(int const*,string const& s){
+	return stoi(s);
+}
+
+bool parse(bool const*,string);
+unsigned parse(unsigned const*,string const&);
+
+template<typename Func,typename T>
+vector<T> sort_by(vector<T> a,Func f){
+	std::sort(begin(a),end(a),[&](auto x,auto y){ return f(x)<f(y); });
+	return a;
+}
+
+template<typename T>
+set<T> to_set(multiset<T> const& a){
+	return std::set<T>(begin(a),end(a));
+}
+
+using Px=double;//probability; should be 0-1.
+
+struct Dist:public map<unsigned,Px>{
+	//models a probability distribution with discrete outcomes.
+	Dist(){
+		(*this)[0]=1;
+	}
+};
+
+Dist mean(vector<Dist> const& a){
+	//just put in each of the items with a reduced probability.
+	Dist r;
+	if(a.size()){
+		r.clear();
+		for(auto d:a){
+			for(auto [v,p]:d){
+				r[v]+=p/a.size();
+			}
+		}
+	}
+	return r;
+}
+
+Dist operator+(Dist const& a,Dist const& b){
+	PRINT(sum(values(a)));
+	PRINT(sum(values(b)));
+
+	
+	Dist r;
+	r.clear();
+	for(auto [av,ap]:a){
+		for(auto [bv,bp]:b){
+			r[av+bv]+=ap*bp;
+		}
+	}
+	PRINT(sum(values(r)));
+	PRINT(r);
+	return r;
+}
+
+Dist to_dist(vector<unsigned> const& a){
+	Dist r;
+	if(a.empty()){
+		return r;
+	}
+
+	r.erase(0);
+	Px per_count=1.0/a.size();
+	for(auto elem:a){
+		r[elem]+=per_count;
+	}
+	return r;
+}
+
+template<size_t N>
+Dist sum(std::array<Dist,N>){
+	nyi
+}
+
+Dist min(unsigned a,Dist b){
+	Dist r;
+	for(auto [val,p]:b){
+		r[min(a,val)]+=p;
+	}
+	return r;
+}
+
+Px operator>=(Dist const& a,unsigned u){
+	auto f=filter(
+		[u](auto p){ return p.first>=u; },
+		to_vec(a)
+	);
+	return sum(seconds(f));
+}
+
+template<typename T>
+std::vector<T> rand(std::vector<T> const*){
+	return mapf(
+		[](auto _){
+			(void)_;
+			return rand((T*)0);
+		},
+		range(rand()%100)
+	);
+}
+
+template<typename Func,typename K,typename V>
+map<K,V> filter_keys(Func f,map<K,V> a){
+	map<K,V> r;
+	for(auto [k,v]:a){
+		if(f(k)){
+			r[k]=v;
+		}
+	}
+	return r;
+}
+
+template<typename K,typename V>
+map<K,V> without_key(K const& k,map<K,V> a){
+	a.erase(k);
+	return a;
+}
+
+template<typename K,typename V>
+set<K> keys(map<K,V> a){
+	return to_set(mapf(
+		[](auto p){ return p.first; },
+		a
+	));
+}
+
+double mean(double a,double b){
+	return (a+b)/2;
+}
+
 //start program-specific code
 
 using Team=Int_limited<1,1000*10>;
+
+Team rand(Team const*){
+	//make a smaller set than is theoretically allowed to make it likely to have the same ones come up
+	return Team{2000+rand()%75};
+}
+
 using Match=Int_limited<1,200>; //qual match number
 
 enum class Alliance{RED,BLUE};
@@ -170,6 +321,8 @@ Alliance parse(Alliance const*,std::string const&)nyi
 Alliance rand(Alliance const*){
 	return (rand()%2)?Alliance::RED:Alliance::BLUE;
 }
+
+vector<Alliance> alliances(){ return {Alliance::RED,Alliance::BLUE}; }
 
 using Climb_assists=Int_limited<0,2>;
 using Balls=Int_limited<0,100>;
@@ -199,6 +352,13 @@ struct Input_row{
 	INPUT_DATA(INST)
 };
 
+bool operator<(Input_row const& a,Input_row const& b){
+	#define X(A,B) if(a.B<b.B) return 1; if(b.B<a.B) return 0;
+	INPUT_DATA(X)
+	#undef X
+	return 0;
+}
+
 #define SHOW(A,B) o<<""#B<<":"<<a.B<<" ";
 
 std::ostream& operator<<(std::ostream& o,Input_row const& a){
@@ -216,15 +376,15 @@ Input_row rand(Input_row const*){
 	#define X(A,B) r.B=rand((A*)0);
 	INPUT_DATA(X)
 	#undef X
+	//put some things in to make it more realistic
+	if(r.climbed) r.park=0;
+	if(r.climb_was_assisted || r.park) r.climb_assists=Climb_assists(0);
+	if(!r.climbed){
+	       r.climb_was_assisted=0;
+	       r.climb_assists=Climb_assists(0);
+	}
 	return r;
 }
-
-int parse(int const*,string const& s){
-	return stoi(s);
-}
-
-bool parse(bool const*,string);
-unsigned parse(unsigned const*,string const&);
 
 vector<Input_row> read_csv(string const& path){
 	ifstream f(path);
@@ -261,8 +421,6 @@ vector<Input_row> read_csv(string const& path){
 	}
 	return r;
 }
-
-using Px=double;//probability; should be 0-1.
 
 static const int TURN_THRESHOLD=29;
 static const int COLOR_PICK_THRESHOLD=49;
@@ -327,7 +485,17 @@ void sanity_check(vector<Input_row> const& a){
 	auto expected_range=range(1,max(matches)+1);
 	for(auto i:expected_range){
 		if(matches.count(Match(i))!=6){
-			//cout<<"Match "<<i<<": "<<matches.count(Match(i))<<" robots\n";
+			cout<<"Match "<<i<<": "<<matches.count(Match(i))<<" entries\n";
+		}
+	}
+
+	//Check that the same robot does not appear more than once in the same match.
+	for(auto [match,data]:group([](auto x){ return x.match; },a)){
+		auto teams=to_multiset(mapf([](auto x){ return x.team; },data));
+		for(auto team:to_set(teams)){
+			if(teams.count(team)!=1){
+				cout<<"Match "<<match<<" Team "<<team<<" appears more than once.\n";
+			}
 		}
 	}
 
@@ -336,7 +504,7 @@ void sanity_check(vector<Input_row> const& a){
 		a
 	);
 
-	for(auto [which,result]:by_alliance){
+	for(auto [which,result]:sorted(to_vec(by_alliance))){
 		auto marker=[=](){
 			stringstream ss;
 			ss<<"Match "<<which.first<<" Alliance "<<which.second<<": ";
@@ -364,7 +532,7 @@ void sanity_check(vector<Input_row> const& a){
 		}
 	}
 
-	for(auto elem:a){
+	for(auto elem:sort_by(a,[](auto x){ return x.match; })){
 		auto marker=[=](){
 			stringstream ss;
 			ss<<"Match "<<elem.match<<" Team "<<elem.team<<": ";
@@ -389,6 +557,13 @@ void sanity_check(vector<Input_row> const& a){
 	}
 }
 
+#define CLIMB_CAPABILITIES(X)\
+	X(Px,climb_unassisted)\
+	X(Px,assist2)\
+	X(Px,assist1)\
+	X(Px,climb_was_assisted)\
+	X(Px,park)
+
 #define ROBOT_CAPABILITIES(X)\
 	X(Px,auto_line)\
 	X(double,auto_low)\
@@ -397,13 +572,11 @@ void sanity_check(vector<Input_row> const& a){
 	X(double,tele_low)\
 	X(double,tele_outer)\
 	X(double,tele_inner)\
+	X(Dist,auto_ball_dist)\
+	X(Dist,tele_ball_dist)\
 	X(Px,wheel_spin)\
 	X(Px,wheel_color)\
-	X(Px,climb_unassisted)\
-	X(Px,assist2)\
-	X(Px,assist1)\
-	X(Px,climb_was_assisted)\
-	X(Px,park)
+	CLIMB_CAPABILITIES(X)
 
 struct Robot_capabilities{
 	ROBOT_CAPABILITIES(INST)
@@ -417,13 +590,23 @@ climb strategy:
 map<Team,park/climb/assisted_climb>
 */
 
+/*
+ * Probability that can get to the bonus objectives
+ * */
+
+pair<Px,Px> bonus_balls_hit(std::array<Dist,3> const& auto_dist,std::array<Dist,3> const& tele_dist){
+	auto auto_dist_trun=min(9,sum(auto_dist));
+	auto total=auto_dist_trun+sum(tele_dist);
+	return make_pair(total>=29,total>=49);
+}
+
 std::ostream& operator<<(std::ostream& o,Robot_capabilities const& a){
 	o<<"Robot_capabilities(";
 	ROBOT_CAPABILITIES(SHOW)
 	return o<<")";
 }
 
-Robot_capabilities operator/(Robot_capabilities a,size_t n){
+/*Robot_capabilities operator/(Robot_capabilities a,size_t n){
 	#define X(A,B) a.B/=n;
 	ROBOT_CAPABILITIES(X)
 	#undef X
@@ -433,6 +616,14 @@ Robot_capabilities operator/(Robot_capabilities a,size_t n){
 Robot_capabilities operator+(Robot_capabilities const& a,Robot_capabilities const& b){
 	Robot_capabilities r;
 	#define X(A,B) r.B=a.B+b.B;
+	ROBOT_CAPABILITIES(X)
+	#undef X
+	return r;
+}*/
+
+Robot_capabilities mean(vector<Robot_capabilities> const& a){
+	Robot_capabilities r;
+	#define X(A,B) r.B=mean(mapf([](auto x){ return x.B; },a));
 	ROBOT_CAPABILITIES(X)
 	#undef X
 	return r;
@@ -446,8 +637,8 @@ bool operator<(Robot_capabilities const& a,Robot_capabilities const& b){
 }
 
 Px wheel_odds(Team team,std::vector<std::vector<Input_row>> const& alliance_results){
-	PRINT(team);
-	PRINT(alliance_results);
+	//PRINT(team);
+	//PRINT(alliance_results);
 
 	//calculate number of time where the alliance had the option to go for a spin
 	auto m=mapf(
@@ -480,8 +671,8 @@ Px wheel_odds(Team team,std::vector<std::vector<Input_row>> const& alliance_resu
 }
 
 Px wheel_color(Team team,std::vector<std::vector<Input_row>> const& alliance_results){
-	PRINT(team);
-	PRINT(alliance_results);
+	//PRINT(team);
+	//PRINT(alliance_results);
 
 	//calculate number of time where the alliance had the option to go for a spin
 	auto m=mapf(
@@ -513,6 +704,55 @@ Px wheel_color(Team team,std::vector<std::vector<Input_row>> const& alliance_res
 	nyi
 }
 
+double assisted2(Team team,std::vector<std::vector<Input_row>> const& alliance_results){
+	auto m=mapf(
+		[=](auto x){
+			auto others_could_take=filter(
+				[=](auto elem){
+					return elem.team!=team && (!elem.climbed || elem.climb_was_assisted);
+				},
+				x
+			);
+			auto available=others_could_take.size()>2;
+			auto f=filter([=](auto x){ return x.team==team; },x);
+			assert(f.size()==1);
+			return make_pair(f[0].climb_assists==2,available);
+		},
+		alliance_results
+	);
+	auto available=sum(seconds(m));
+	if(!available) return 0;
+	return (0.0+sum(firsts(m)))/available;
+}
+
+double assisted1(Team team,std::vector<std::vector<Input_row>> const& alliance_results){
+	auto m=mapf(
+		[=](auto x){
+			auto others_could_take=filter(
+				[=](auto elem){
+					return elem.team!=team && (!elem.climbed || elem.climb_was_assisted);
+				},
+				x
+			);
+			auto available=others_could_take.size();
+			auto f=filter([=](auto x){ return x.team==team; },x);
+			assert(f.size()==1);
+			return make_pair(f[0].climb_assists==1,available);
+		},
+		alliance_results
+	);
+	auto available=sum(seconds(m));
+	if(!available) return 0;
+	return (0.0+sum(firsts(m)))/available;
+}
+
+Px park(std::vector<Input_row> const& a){
+	auto available=filter([](auto x){ return !x.climbed; },a).size();
+	if(!available) return 0;
+	auto done=sum(mapf([](auto x){ return x.park; },a));
+	return (done+0.0)/available;
+}
+
 map<Team,Robot_capabilities> robot_capabilities(vector<Input_row> const& in){
 	vector<vector<Input_row>> alliance_results=values(group(
 		[](auto x){ return make_pair(x.match,x.alliance); },
@@ -539,13 +779,21 @@ map<Team,Robot_capabilities> robot_capabilities(vector<Input_row> const& in){
 					mean_d(mapf([](auto x){ return x.tele_low; },f)),
 					mean_d(mapf([](auto x){ return x.tele_outer; },f)),
 					mean_d(mapf([](auto x){ return x.tele_inner; },f)),
+					to_dist(mapf(
+						[](auto x)->unsigned{ return x.auto_low+x.auto_outer+x.auto_inner; },
+						f
+					)),
+					to_dist(mapf(
+						[](auto x)->unsigned{ return x.tele_low+x.tele_outer+x.tele_inner; },
+						f
+					)),
 					wheel_odds(team,this_alliance),
 					wheel_color(team,this_alliance),
-					0,//climb unassisted
-					0,//assisted2
-					0,//assisted1
-					0,//climb was assisted
-					0//park
+					mean_d(mapf([](auto x){ return x.climbed && !x.climb_was_assisted; },f)),//climb unassisted
+					assisted2(team,this_alliance),
+					assisted1(team,this_alliance),
+					mean_d(mapf([](auto x){ return x.climbed && x.climb_was_assisted; },f)),//climb was assisted
+					park(f) //park
 				}
 			);
 		},
@@ -553,15 +801,57 @@ map<Team,Robot_capabilities> robot_capabilities(vector<Input_row> const& in){
 	));
 }
 
-template<typename T>
-std::vector<T> rand(std::vector<T> const*){
-	return mapf(
-		[](auto _){
-			(void)_;
-			return rand((T*)0);
-		},
-		range(rand()%20)
-	);
+auto wheel_spins(vector<Input_row> const& a){
+	return sum(mapf([](auto x){ return x.wheel_spin; },a));
+}
+
+unsigned climb_assists_given(vector<Input_row> const& a){
+	return sum(mapf([](auto x){ return x.climb_assists; },a));
+}
+
+unsigned climb_assists_rx(vector<Input_row> const& a){
+	return sum(mapf([](auto x){ return x.climb_was_assisted; },a));
+}
+
+std::vector<Input_row> rand(std::vector<Input_row> const*){
+	std::vector<Input_row> r;
+	for(auto match:range(1,50)){
+		std::vector<Input_row> match_data;
+		for(auto alliance:alliances()){
+			auto v=mapf(
+				[=](auto _){
+					(void)_;
+					auto r=rand((Input_row*)0);
+					r.match=Match(match);
+					r.alliance=alliance;
+					return r;
+				},
+				range(3)
+			);
+			while(wheel_spins(v)>1){
+				v[rand()%3].wheel_spin=0;
+			}
+
+			while(climb_assists_given(v)>climb_assists_rx(v)){
+				auto& x=v[rand()%3].climb_assists;
+				if(x) x--;
+			}
+
+			while(climb_assists_rx(v)>climb_assists_given(v)){
+				v[rand()%3].climb_was_assisted=0;
+			}
+
+			match_data|=v;
+		}
+
+		//make sure all three robots on the alliance are different
+		while(teams(match_data).size()<6){
+			match_data[rand()%6].team=rand((Team*)0);
+		}
+
+		r|=match_data;
+	}
+	return r;
 }
 
 void show(std::map<Team,Robot_capabilities> const& data){
@@ -597,18 +887,97 @@ void show(std::map<Team,Robot_capabilities> const& data){
 	write_file("robot_capabilities.html",s);
 }
 
-template<typename Func,typename K,typename V>
-map<K,V> filter_keys(Func f,map<K,V> a){
-	map<K,V> r;
-	for(auto [k,v]:a){
-		if(f(k)){
-			r[k]=v;
+using Alliance_capabilities=std::array<Robot_capabilities,3>;
+
+enum Climb_strategy{
+	NONE,
+	PARK,
+	CLIMB_SELF,
+	CLIMB_ASSISTED
+};
+
+vector<Climb_strategy> climb_strategies(){
+	return vector<Climb_strategy>{
+		Climb_strategy::NONE,
+		Climb_strategy::PARK,
+		Climb_strategy::CLIMB_SELF,
+		Climb_strategy::CLIMB_ASSISTED
+	};
+}
+
+std::ostream& operator<<(std::ostream& o,Climb_strategy const& a){
+	#define X(A) if(a==Climb_strategy::A) return o<<""#A;
+	X(NONE) X(PARK) X(CLIMB_SELF) X(CLIMB_ASSISTED)
+	#undef X
+	assert(0);
+}
+
+using Alliance_climb_strategy=std::array<Climb_strategy,3>;
+
+vector<Alliance_climb_strategy> alliance_climb_strategies(){
+	vector<Alliance_climb_strategy> r;
+	for(auto a:climb_strategies()){
+		for(auto b:climb_strategies()){
+			for(auto c:climb_strategies()){
+				r|=Alliance_climb_strategy{a,b,c};
+			}
 		}
 	}
 	return r;
 }
 
-using Alliance_capabilities=std::array<Robot_capabilities,3>;
+struct Climb_capabilities{
+	CLIMB_CAPABILITIES(INST)
+};
+
+std::ostream& operator<<(std::ostream& o,Climb_capabilities const& a){
+	o<<"Climb_capabilities( ";
+	#define X(A,B) o<<""#B<<":"<<a.B<<" ";
+	CLIMB_CAPABILITIES(X)
+	#undef X
+	return o<<")";
+}
+
+using Alliance_climb_capabilities=std::array<Climb_capabilities,3>;
+
+double climb_points(Alliance_climb_capabilities const& cap,Alliance_climb_strategy const& strat){
+	auto z=zip(cap,strat);
+	double park_points=0;
+	vector<double> assists_available;
+	vector<double> assists_req;
+	double climbed=0;
+	for(auto [cap1,strat1]:z){
+		switch(strat1){
+			case NONE:
+				break;
+			case PARK:
+				park_points+=5*cap1.park;
+				break;
+			case CLIMB_SELF:
+				climbed+=cap1.climb_unassisted;
+				break;
+			case CLIMB_ASSISTED:
+				assists_req|=cap1.climb_was_assisted;
+				break;
+			default:
+				assert(0);
+		}
+	}
+	for(auto [a,b]:zip(
+		reversed(sorted(assists_available)),
+		reversed(sorted(assists_req))
+	)){
+		climbed+=mean(a,b);//geometric mean might be better
+	}
+	return park_points+climbed*25;
+}
+
+double climb_points(Alliance_climb_capabilities const& cap){
+	return max(mapf(
+		[=](auto x){ return climb_points(cap,x); },
+		alliance_climb_strategies()
+	));
+}
 
 double expected_score(Alliance_capabilities const& a){
 	//at some point might want to make this fancier and do things like scoring rates based on the amount of time
@@ -616,6 +985,8 @@ double expected_score(Alliance_capabilities const& a){
 	//also, it would be good if had some idea of what the distribution of # of balls was so that could estimate
 	//how likely it would be to get alliances to the different thresholds rather than just having a binary
 	//yes/no on them.
+	auto auto_line_points=5*sum(mapf([](auto x){ return x.auto_line; },a));
+
 	auto auto_low=sum(mapf([](auto x){ return x.auto_low; },a));
 	auto auto_outer=sum(mapf([](auto x){ return x.auto_outer; },a));
 	auto auto_inner=sum(mapf([](auto x){ return x.auto_inner; },a));
@@ -645,23 +1016,26 @@ double expected_score(Alliance_capabilities const& a){
 		return 0;
 	}();
 
-	auto hang_points=0;//FIXME
+	//hang points
+	//hang strategies:
+	//does each one want to hang?
+	//for each of them, assume that they have y/n for attempt to do something for each team
+	//then for each of those categories, choose between park/climb/get assisted (if available)
+	//
+	auto hang_points=climb_points(
+		mapf(
+			[](auto x){
+				Climb_capabilities r;
+				#define X(A,B) r.B=x.B;
+				CLIMB_CAPABILITIES(X)
+				#undef X
+				return r;
+			},
+			a
+		)
+	);
 
-	return ball_points+spin_points+color_pick_points+hang_points;
-}
-
-template<typename K,typename V>
-map<K,V> without_key(K const& k,map<K,V> a){
-	a.erase(k);
-	return a;
-}
-
-template<typename K,typename V>
-set<K> keys(map<K,V> a){
-	return to_set(mapf(
-		[](auto p){ return p.first; },
-		a
-	));
+	return auto_line_points+ball_points+spin_points+color_pick_points+hang_points;
 }
 
 using Picklist=vector<
@@ -687,7 +1061,7 @@ void show_picklist(Team picker,Picklist const& a){
 			tag("table border",
 				tr(
 					tag("th colspan=2","First pick")+
-					th("Second pick")
+					tag("th colspan=22","Second pick")
 				)+
 				tr(
 					th("Rank")+
@@ -701,10 +1075,10 @@ void show_picklist(Team picker,Picklist const& a){
 						return tr(
 							th(i)
 							+show_box(fp)
-							+join(mapf(show_box,second))
+							+join(mapf(show_box,take(22,second)))
 						);
 					},
-					enumerate_from(1,a)
+					enumerate_from(1,take(16,a))
 				))
 			)
 		)
@@ -724,6 +1098,7 @@ Picklist make_picklist(Team picking_team,map<Team,Robot_capabilities> a){
 	auto x=reversed(sorted(mapf(
 		[=](auto p){
 			auto [t1,t1_cap]=p;
+			(void)t1;
 			Alliance_capabilities cap{picking_cap,t1_cap,{}};
 			return make_pair(expected_score(cap),t1_cap);
 		},
@@ -754,7 +1129,7 @@ Picklist make_picklist(Team picking_team,map<Team,Robot_capabilities> a){
 		others
 	)));
 
-	PRINT(first_picks);
+	//PRINT(first_picks);
 
 	auto second_picks=to_map(mapf(
 		[&](auto t1)->pair<Team,vector<pair<double,Team>>>{
@@ -796,7 +1171,7 @@ void run(Team team,std::optional<string> const& path){
 	//check that the target team is in the data.
 	auto t=teams(data);
 	if(!t.count(team)){
-		cout<<"Warning: Did not find picking team in the data.  Adding zeros.\n";
+		cout<<"Warning: Did not find picking team ("<<team<<") in the data.  Adding zeros.\n";
 		rc[team]={};
 	}
 
