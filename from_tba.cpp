abccd200fce45ec05f6e33647a7d1905f6f677eb
Eric Rippey <rippey.e@gmail.com>
2020-02-27 13:12:06 -0800

    Robot capability estimates based on data from The Blue Alliance

--- from_tba.cpp
+++ from_tba.cpp
@@ -6,6 +6,32 @@
 
 using namespace std;
 
+template<typename T>
+T min(T a,T b,T c){ return std::min(a,std::min(b,c)); }
+
+template<typename A,typename B,typename C>
+int sum(std::tuple<A,B,C>)nyi
+
+template<typename A,typename B,typename C>
+std::vector<std::tuple<A,B,C>> zip(std::vector<A> const& a,std::vector<B> const& b,std::vector<C> const& c){
+	return mapf(
+		[&](auto i){
+			return make_tuple(a[i],b[i],c[i]);
+		},
+		range(min(a.size(),b.size(),c.size()))
+	);
+}
+
+template<typename T>
+std::vector<T> to_vec(std::multiset<T> a){
+	return std::vector<T>(a.begin(),a.end());
+}
+
+auto mean(multiset<unsigned> a){ return mean(to_vec(a)); }
+
+template<typename T>
+auto mean_d(multiset<T> a){ return mean_d(to_vec(a)); }
+
 template<typename... A>
 std::ostream& operator<<(std::ostream& o,std::variant<A...> const& a){
         std::visit([&](auto &&elem){ o<<elem; },a);
@@ -65,9 +91,17 @@ void print_r(unsigned i,std::optional<T> const& a){
 }
 
 #define INFO_ITEMS(X)\
-	X(multiset<tba::Endgame>,endgame)\
 	X(multiset<tba::Init_line>,init_line)\
-	X(multiset<int>,alliance_balls_scored)
+	X(vector<unsigned>,auto_low)\
+	X(vector<unsigned>,auto_outer)\
+	X(vector<unsigned>,auto_inner)\
+	X(vector<unsigned>,tele_low)\
+	X(vector<unsigned>,tele_outer)\
+	X(vector<unsigned>,tele_inner)\
+	X(multiset<bool>,wheel_spin)\
+	X(multiset<bool>,wheel_color)\
+	X(multiset<tba::Endgame>,endgame)\
+	X(multiset<bool>,balance)
 
 struct Info{
 	INFO_ITEMS(INST)
@@ -87,12 +121,91 @@ void print_r(unsigned indent,Info const& a){
 	#undef X
 }
 
-int main1(){
+template<typename T>
+std::vector<T> operator/(std::vector<T> a,int x){
+	for(auto& elem:a){
+		elem/=x;
+	}
+	return a;
+}
+
+Dist dist_third(vector<unsigned> const& a,vector<unsigned> const& b,vector<unsigned> const& c){
+	auto totals=MAP(sum,zip(a,b,c))/3;//note that the fact that these are all rounded down introduces some bias.
+	Dist r;
+	r.clear();
+	for(auto elem:totals){
+		r[elem]+=1.0/totals.size();
+	}
+	return r;
+}
+
+multiset<unsigned>& operator|=(multiset<unsigned>& a,int b){
+	assert(b>=0);
+	a.insert(b);
+	return a;
+}
+
+Team parse_team(tba::Team_key const& a){
+	auto s=a.str();
+	assert(s.size()>=4);
+	assert(s[0]=='f');
+	assert(s[1]=='r');
+	assert(s[2]=='c');
+	return Team{atoi(s.c_str()+3)};
+}
+
+void help(){
+	cout<<"Arguments:\n";
+	cout<<"--event <EVENT_KEY>\n";
+	cout<<"\tMandatory - the event code.  For example, \"2020isde1\".\n";
+	cout<<"--team <TEAM_NUMBER>\n";
+	cout<<"\tDefaults to 1425.\n";
+	cout<<"--help\n";
+	cout<<"\tShow this message.\n";
+	exit(0);
+}
+
+struct Args{
+	Team team;
+	tba::Event_key event;
+};
+
+Args parse_args(int argc,char **argv){
+	(void)argc;
+	Team team{1425};
+	optional<tba::Event_key> event;
+	for(auto at=argv+1;*at;++at){
+		if(string(*at)=="--team"){
+			at++;
+			assert(at);
+			team=Team{atoi(*at)};
+		}else if(string(*at)=="--event"){
+			at++;
+			assert(at);
+			event=tba::Event_key{*at};
+		}else if(string(*at)=="--help"){
+			help();
+		}else{
+			cout<<"Unrecognized argument.\n";
+			help();
+		}
+	}
+	if(!event){
+		cout<<"Error: Valid event must be specified.\n";
+		exit(1);
+	}
+	return Args{team,*event};
+}
+
+int main1(int argc,char **argv){
+	cout<<"a0\n";
+	Args args=parse_args(argc,argv);
+	cout<<"a1\n";
 	std::ifstream f("../tba/auth_key");
 	std::string tba_key;
 	getline(f,tba_key);
 	tba::Cached_fetcher cf{tba::Fetcher{tba::Nonempty_string{tba_key}},tba::Cache{}};
-
+	cout<<"b1\n";
 	tba::Event_key event_key{"2020isde1"};
 	auto found=event_matches(cf,event_key);
 	PRINT(found.size());
@@ -100,6 +213,7 @@ int main1(){
 
 	map<tba::Team_key,Info> info;
 
+	//map<Input_row> rows;
 	for(auto match:found){
 		/*#define X(A,B) cout<<""#B<<"("#A<<")"<<"\n"; print_r(1,match.B);
 		TBA_MATCH(X)
@@ -121,6 +235,7 @@ int main1(){
 			//print_r(0,result);
 
 			assert(alliance.team_keys.size()==3);
+
 			auto& t1=info[alliance.team_keys[0]];
 			auto& t2=info[alliance.team_keys[1]];
 			auto& t3=info[alliance.team_keys[2]];
@@ -133,14 +248,31 @@ int main1(){
 			t2.endgame|=result.endgameRobot2;
 			t3.endgame|=result.endgameRobot3;
 
-			auto auto_balls_scored=result.autoCellsBottom+result.autoCellsOuter+result.autoCellsInner;;
+			vector<Info*> teams;
+			teams|=&t1;
+			teams|=&t2;
+			teams|=&t3;
+
+			for(auto t:teams){
+				assert(t);
+				t->auto_low|=result.autoCellsBottom;
+				t->auto_outer|=result.autoCellsOuter;
+				t->auto_inner|=result.autoCellsInner;
+				t->tele_low|=result.teleopCellsBottom;
+				t->tele_outer|=result.teleopCellsOuter;
+				t->tele_inner|=result.teleopCellsInner;
+				t->wheel_spin|=result.stage2Activated;
+				t->wheel_color|=result.stage3Activated;
+				t->balance|=(result.endgameRungIsLevel==tba::Rung_level::IsLevel);
+			}
+			/*auto auto_balls_scored=result.autoCellsBottom+result.autoCellsOuter+result.autoCellsInner;;
 			auto teleop_balls_scored=result.teleopCellsBottom+result.teleopCellsOuter+result.teleopCellsInner;
 
 			//this is only the number that could be towards advancing levels
 			auto balls_scored=min(9,auto_balls_scored)+teleop_balls_scored;
 			t1.alliance_balls_scored|=balls_scored;
 			t2.alliance_balls_scored|=balls_scored;
-			t3.alliance_balls_scored|=balls_scored;
+			t3.alliance_balls_scored|=balls_scored;*/
 		};
 
 		analyze_alliance(
@@ -156,35 +288,55 @@ int main1(){
 	//PRINT(info.size());
 	//print_r(info);
 
-	auto m=mapf(
-		[](auto p)->Robot_capabilities{
+	auto m=to_map(mapf(
+		[](auto p)->std::pair<Team,Robot_capabilities>{
 			auto [team,data]=p;
-			PRINT(team);
-			PRINT(data);
-			/*			        X(Px,auto_line)\
-        X(double,auto_low)\
-        X(double,auto_outer)\
-        X(double,auto_inner)\
-        X(double,tele_low)\
-        X(double,tele_outer)\
-        X(double,tele_inner)\
-        X(Dist,auto_ball_dist)\
-        X(Dist,tele_ball_dist)\
-        X(Px,wheel_spin)\
-        X(Px,wheel_color)\
-        CLIMB_CAPABILITIES(X)
-*/
-			nyi
+			//PRINT(team);
+			//PRINT(data);
+			auto cap=Robot_capabilities{
+				mean(mapf([](auto x){ return x==tba::Init_line::Exited; },data.init_line)),
+				mean_d(data.auto_low)/3,
+				mean_d(data.auto_outer)/3,
+				mean_d(data.auto_inner)/3,
+				mean_d(data.tele_low)/3,
+				mean_d(data.tele_outer)/3,
+				mean_d(data.tele_inner)/3,
+				dist_third(data.auto_low,data.auto_outer,data.auto_inner),
+				dist_third(data.tele_low,data.tele_outer,data.tele_inner),
+				mean(data.wheel_spin),
+				mean(data.wheel_color),
+
+				//climbed self
+				mean(mapf([](auto x){ return x==tba::Endgame::Hang; },data.endgame)),
+
+				0, //assist2
+				0, //assist1
+				0, //climb was assisted
+
+				mean(mapf([](auto x){ return x==tba::Endgame::Park; },data.endgame)),
+				mean(data.balance)
+			};
+			return make_pair(parse_team(team),cap);
 		},
 		info
-	);
-	print_lines(m);
+	));
+
+	//print_lines(m);
+
+	//now show the extemes for each of the values
+	#define X(A,B) {\
+		auto data=mapf([](auto x){ return x.B; },values(m));\
+		cout<<""#B<<":"<<min(data)<<" "<<max(data)<<" "<<mean(data)<<"\n";\
+	}
+	ROBOT_CAPABILITIES(X)
+	#undef X
 	nyi
+	return 0;
 }
 
-int main(){
+int main(int argc,char **argv){
 	try{
-		return main1();
+		return main1(argc,argv);
 	}catch(std::string const& s){
 		std::cout<<"Caught: "<<s<<"\n";
 	}
